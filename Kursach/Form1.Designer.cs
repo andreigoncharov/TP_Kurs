9e293c361a1dd5fb0b9d0016aacdaa150b221d06
andreigoncharov <49717261+andreigoncharov@users.noreply.github.com>
2021-05-11 16:08:06 +0300

    v.1.0

---
+++ Kursach/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Kursach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.expressionTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.historyRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.errorsRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // expressionTextBox
+            // 
+            this.expressionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.expressionTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.expressionTextBox.Location = new System.Drawing.Point(12, 47);
+            this.expressionTextBox.Name = "expressionTextBox";
+            this.expressionTextBox.Size = new System.Drawing.Size(365, 31);
+            this.expressionTextBox.TabIndex = 0;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.calculateButton.BackColor = System.Drawing.Color.White;
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.calculateButton.Location = new System.Drawing.Point(383, 47);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(114, 31);
+            this.calculateButton.TabIndex = 1;
+            this.calculateButton.Text = "Вычислить";
+            this.calculateButton.UseVisualStyleBackColor = false;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // historyRichTextBox
+            // 
+            this.historyRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.historyRichTextBox.Enabled = false;
+            this.historyRichTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.historyRichTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.historyRichTextBox.Location = new System.Drawing.Point(296, 172);
+            this.historyRichTextBox.Name = "historyRichTextBox";
+            this.historyRichTextBox.ReadOnly = true;
+            this.historyRichTextBox.Size = new System.Drawing.Size(201, 177);
+            this.historyRichTextBox.TabIndex = 2;
+            this.historyRichTextBox.Text = "";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultTextBox.Location = new System.Drawing.Point(132, 93);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(365, 31);
+            this.resultTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Результат:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(297, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "История:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(17, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ошибки:";
+            // 
+            // errorsRichTextBox
+            // 
+            this.errorsRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.errorsRichTextBox.Enabled = false;
+            this.errorsRichTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.errorsRichTextBox.ForeColor = System.Drawing.Color.Red;
+            this.errorsRichTextBox.Location = new System.Drawing.Point(16, 172);
+            this.errorsRichTextBox.Name = "errorsRichTextBox";
+            this.errorsRichTextBox.ReadOnly = true;
+            this.errorsRichTextBox.Size = new System.Drawing.Size(201, 177);
+            this.errorsRichTextBox.TabIndex = 6;
+            this.errorsRichTextBox.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(282, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Выражение в инфиксной форме";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(322, 29);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(54, 13);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Очистить";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(163, 156);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(54, 13);
+            this.linkLabel2.TabIndex = 10;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Очистить";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(443, 156);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(54, 13);
+            this.linkLabel3.TabIndex = 11;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "Очистить";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(509, 361);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.errorsRichTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.historyRichTextBox);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.expressionTextBox);
+            this.MinimumSize = new System.Drawing.Size(525, 400);
+            this.Name = "Form1";
+            this.Text = "Преобразование инфиксной формы записи выражений в программу";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox expressionTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.RichTextBox historyRichTextBox;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox errorsRichTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+    }
+}
+
---
+++ Kursach/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kursach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            String expressionText = expressionTextBox.Text.ToString();
+            if (CheckExpressionText(expressionText))
+            {
+                Queue<Char> result = new Parser().getPostFixEx(expressionText);
+                String textResult = "";
+                while (true)
+                {
+                    if (result.Count != 0)
+                    {
+                        textResult += Convert.ToString(result.Dequeue());
+                    }
+                    else break;
+                }
+                resultTextBox.Text = textResult;
+                historyRichTextBox.Text += textResult + "\n";
+            }
+            else
+                MessageBox.Show("Ошибка в выражении!");
+
+        }
+
+        public bool CheckExpressionText(String text)
+        {
+            if (text.Length <= 2)
+            {
+                errorsRichTextBox.Text += "Слишком короткое выражение!\n";
+                return false;
+            }
+            if(Regex.IsMatch(text, @"^[=!@|,.&?^%$]+$") && text.All(c => Char.IsLetterOrDigit(c) || c == '_'))
+            {
+                errorsRichTextBox.Text += "Выражение содержит недопустимые символы!\n";
+                return false;
+            }
+            else if (text.Contains("("))
+            {
+                int count1 = 0, count2 = 0;
+                foreach (Char c in text)
+                {
+                    if (c.Equals('(')) count1 += 1;
+                    if (c.Equals(')')) count2 += 1;
+                }
+                if (count1 > count2)
+                {
+                    errorsRichTextBox.Text += "Нехватает закрывающейся скобки\n";
+                    return false;
+                }
+                else if (count1 < count2)
+                {
+                    errorsRichTextBox.Text += "Нехватает открывающейся скобки\n";
+                    return false;
+                }
+                else return true;
+            }
+            else return true;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            expressionTextBox.Text = "";
+            resultTextBox.Text = "";
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            errorsRichTextBox.Text = "";
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            historyRichTextBox.Text = "";
+        }
+    }
+    class Parser
+    {
+        Queue<Char> operands = new Queue<Char>();
+        Stack<Char> operators = new Stack<Char>();
+
+        public Queue<Char> getPostFixEx(string str)//публичный метод класса Parser
+        {
+            foreach (char c in str)
+            {
+                if (c.Equals('(')) push(c);
+                if (c.Equals(')')) pop();
+                if (Char.IsLetter(c) || Char.IsDigit(c)) addQueue(c);
+                if(c.Equals('+') || c.Equals('-'))
+                {
+                    if (operators.Count != 0)
+                    {
+                        Char peek = Convert.ToChar(operators.Peek());
+                        if (peek.Equals('('))
+                        {
+                            push(c);
+                        }
+
+                        else if (peek.Equals('/') || peek.Equals('*'))
+                        {
+                            pop();
+                            push(c);
+                        }
+                        else
+                        {
+                            addQueue(operators.Pop());
+                            push(c);
+                        }
+                    }else push(c);
+                }
+                if (c.Equals('*') || c.Equals('/'))
+                {
+                    if (operators.Count != 0)
+                    {
+                        Char peek = Convert.ToChar(operators.Peek());
+                        if (peek.Equals('*') || peek.Equals('/'))
+                        {
+                            pop();
+                        }
+                    }
+                    push(c);
+                }
+
+            }
+            if (operators.Count != 0)
+            {
+                bool b = true;
+                while (b)
+                {
+                    if (operators.Count != 0)
+                    {
+                        Char peek = Convert.ToChar(operators.Peek());
+                        if(!peek.Equals('('))
+                            operands.Enqueue(Convert.ToChar(operators.Pop()));
+                    }
+                    else
+                        b = false;
+                }
+            }
+                return operands;
+        }
+        public void pop()
+        {
+            while (true)
+            {
+                if (operators.Count != 0)
+                {
+                    Char c = Convert.ToChar(operators.Pop());
+                    if (c.Equals('('))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        operands.Enqueue(c);
+                    }
+                }
+                else break;
+            }
+        }
+
+        public void addQueue(Char item)
+        {
+            operands.Enqueue(item);
+        }
+
+        public void push(Char item)
+        {
+            operators.Push(item);
+        }
+    }
+}
